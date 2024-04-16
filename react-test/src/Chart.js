6f8a630278db2c6f9649afc1a6c753ebf73411a8
yeojin10044 <153582138+yeojin10044@users.noreply.github.com>
2024-04-16 19:01:47 +0900

    챗봇 , 네비바 완료

---
+++ react-test/src/Chart.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Chart() {
+  return (
+    <img src="/chart.png" alt="chart" width="500" height="500" />
+  );
+}
+
+export default Chart;
---
+++ react-test/src/Map.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Map() {
+  return (
+    <img src="/MAP.jpg" alt="MAP" width="700" height="700" />
+  );
+}
+
+export default Map;
