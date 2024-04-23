99e70fa8d48b07e4d2cb2764401b1c42fa337471
yeojin10044 <153582138+yeojin10044@users.noreply.github.com>
2024-04-23 17:23:52 +0900

    axios로 데이터 받기

--- react-test/src/App.js
+++ react-test/src/App.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Link, Routes, Route } from 'react-router-dom';
 import './App.css';
+import axios from 'axios'
 
 import MapImage from './commponent/MapImage';
 import ChartImage from './commponent/ChartImage';
@@ -10,6 +11,19 @@ import LoginForm from './commponent/LoginForm';
 function App() {
   const labels = ['데이터1', '데이터2', '데이터3'];
   const data = [1, 2, 3];
+  
+  // axios로 받아온 데이터를 저장할 state
+  const [apiData, setApiData] = useState(null);
+  
+  const fetchData = () => {
+    axios.get('http://apis.data.go.kr/5080000/polcsttnCctvSttuService/getPolcsttnCctvSttu')
+      .then((결과) => {
+        console.log(결과.data);
+      })
+      .catch((error) => {
+        console.log('실패함', data);
+      });
+  };
 
   return (
     <Router>
@@ -27,12 +41,21 @@ function App() {
         {/* 메뉴별 화면 나오는곳 */}
         <Routes>
           <Route path="/map" element={<MapImage src="/MAP.jpg" />} />
-          
-          {/* 올바른 props 전달 */}
           <Route path="/chart" element={<ChartImage labels={labels} dataProp={data} />} />
           <Route path="/login" element={<LoginForm />} />
         </Routes>
-         
+
+        {/* 데이터 받아오는 버튼 */}
+        <button onClick={fetchData}>데이터 받아오기</button>
+        
+        {/* 받아온 데이터를 출력 */}
+        {apiData && (
+          <div>
+            <h4>받아온 데이터:</h4>
+            <pre>{JSON.stringify(apiData, null, 2)}</pre>
+          </div>
+        )}
+
         {/* 쳇봇 나오는곳 */}
         <Chatbot />
       </div>
