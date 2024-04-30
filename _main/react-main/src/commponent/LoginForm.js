13fe8f0b6213842f57bc57299ee731b6876acbdf
WonBinPark <57087624+ParkWonBin@users.noreply.github.com>
2024-04-30 10:24:12 +0900

    init_react
    redux 적용은 아직

---
+++ _main/react-main/src/commponent/LoginForm.js
@@ -0,0 +1,47 @@
+import React, { useState } from 'react';
+
+function LoginForm() {
+  const [getId, setId] = useState('');
+  const [getPassword, setPassword] = useState('');
+
+  const handleLogin = () => {
+    // 로그인 로직을 여기에 작성합니다.
+    console.log('로그인 시도:', getId, getPassword);
+    alert(`${getId}\n ${getPassword}`)
+  };
+
+  const handleSignUp = () => {
+    // 회원가입 로직 또는 페이지로의 이동을 여기에 작성합니다.
+    console.log('회원가입 페이지로 이동');
+  };
+
+  
+  return (
+    <div className='login-form-container'>
+      <div className='input-container'>
+        <label htmlFor="id">ID :</label>
+        <input className='input-field'
+          type="text"
+          id="id"
+          value={getId}
+          onChange={(e) => setId(e.target.value)}
+        />
+      </div>
+      <div className='input-container'>
+        <label htmlFor="password">PW :</label>
+        <input className='input-field'
+          type="password"
+          id="password"
+          value={getPassword}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </div>
+      <div className='button-container'>
+        <button className='login-button' onClick={handleLogin}>로그인</button>
+        <button className='signup-button' onClick={handleSignUp}>회원가입</button>
+      </div>
+    </div>
+  );
+}
+
+export default LoginForm;
---
+++ _main/react-main/src/commponent/MapImage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function MapImage(props) {
+  return (
+    <img src={props.src} alt="이미지 없음 ㅠㅠ" width="700" height="700" />
+  );
+}
+
+export default MapImage;
