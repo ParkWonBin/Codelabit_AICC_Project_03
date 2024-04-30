416405a3c1fc50f0f3bb2c1cbdf48c1e9468a6cd
yeojin10044 <153582138+yeojin10044@users.noreply.github.com>
2024-04-30 10:33:13 +0900

    최신 수정사항 적용

--- _main/react-main/src/App.js
+++ _main/react-main/src/App.js
@@ -1,35 +1,67 @@
+import React, { useState } from 'react';
+import { Link, Routes, Route } from 'react-router-dom';
 import './App.css';
-import React from 'react';
-import { Link, Routes, Route } from 'react-router-dom'; // index.js에서 
+import axios from 'axios'
 
-// commponets 불러오기
 import MapImage from './commponent/MapImage';
 import ChartImage from './commponent/ChartImage';
-import LoginForm from './commponent/LoginForm';
 import Chatbot from './commponent/chatbot';
-
+import LoginForm from './commponent/LoginForm';
 
 function App() {
-  return (
-    <div className="App">
-    
-      <nav className="navbar">
-        <h3>AICC 3차 프로젝트 1팀!</h3>
-        <ul>
-          <li><Link to="/map">MAP</Link></li>
-          <li><Link to="/chart">chart</Link></li>
-          <li><Link to="/login">Login</Link></li>
-        </ul>
-      </nav>
+  const labels = ['데이터1', '데이터2', '데이터3'];
+  const data = [1, 2, 3];
+  
+  // axios로 받아온 데이터를 저장할 state
+  const [apiData, setApiData] = useState(null);
+  
+  const fetchData = () => {
+    axios.get('http://apis.data.go.kr/5080000/polcsttnCctvSttuService/getPolcsttnCctvSttu')
+      .then((결과) => {
+        // 받아온 데이터를 state에 저장
+        setApiData(결과.data);
+      })
+      .catch((data) => {
+        console.log('실패함', data);
+      });
+  };
 
-      <Routes>
-        <Route path="/map" element={<MapImage src='/MAP.jpg' />} />
-        <Route path="/chart" element={<ChartImage src='/chart.png' />} />
-        <Route path="/login" element={<LoginForm />} />
-      </Routes>
+  return (
+
+      <div className="App">
+        {/* 네비게이션 */}
+        <nav className="navbar">
+          <h3>AICC 3차 프로젝트 1팀!</h3>
+          <ul>
+            <li><Link to="/map">MAP</Link></li>
+            <li><Link to="/chart">chart</Link></li>
+            <li><Link to="/login">Login</Link></li>
+          </ul>
+        </nav>
+
+         {/* 메뉴별 화면 나오는곳 */}
+         <Routes>
+          <Route path="/map" element={<MapImage src="/MAP.jpg" />} />
+          <Route path="/chart" element={<ChartImage labels={labels} dataProp={data} />} />
+          {/* LoginForm에 fetchData와 apiData 전달 */}
+          <Route path="/login" element={<LoginForm fetchData={fetchData} apiData={apiData} />} />
+        </Routes>
+
+        {/* 데이터 받아오는 버튼 (LoginForm으로 옮기지 않음) */}
+        <button onClick={fetchData}>데이터 받아오기</button>
         
-      <Chatbot/>
-    </div>
+        {/* 받아온 데이터를 출력 (LoginForm으로 옮기지 않음) */}
+        {apiData && (
+          <div>
+            <h4>받아온 데이터:</h4>
+            <pre>{JSON.stringify(apiData, null, 2)}</pre>
+          </div>
+        )}
+
+        {/* 쳇봇 나오는곳 */}
+        <Chatbot />
+      </div>
+    // </Router>
   );
 }
 
--- _main/react-main/src/commponent/LoginForm.js
+++ _main/react-main/src/commponent/LoginForm.js
@@ -1,47 +1,117 @@
+// LoginForm.js
+
 import React, { useState } from 'react';
+import axios from 'axios'
+import './LoginForm.css';
 
 function LoginForm() {
+  // 리덕스로 getLoginUserName,setLoginUserName 요거 넣어서 관리해보기!!! (이번주 목표!!!)
+  const [getLoginUserName,setLoginUserName] = useState('');
+
+
   const [getId, setId] = useState('');
   const [getPassword, setPassword] = useState('');
 
-  const handleLogin = () => {
-    // 로그인 로직을 여기에 작성합니다.
-    console.log('로그인 시도:', getId, getPassword);
-    alert(`${getId}\n ${getPassword}`)
+ // 제목 : 로그인 처리용 함수 생성
+ // 요약 : id, pw받아서 성공여부를 true false로 리턴함.
+ // 비고 : 비동기 함수(async)라서 사용 시 await을 써줘야함.
+const tryLogin = async (id, pw) => {
+
+  // 특이사항: axios에서 get, post로 넣으면 요청이 들어갔는데. 여기에서는 create를 썼다.
+  // 접근방법: creat된 애가 뭐냐? 라고하면, 서버의 baseURL을 갖고, 요청의 구조(서식)을 정의한 것.
+  // 접근예시: 정체가 뭐냐???? => 뭐하는 녀석이냐?? => 그 녀석과 관련된 것들이 뭐냐?? => 그 쓰임세를 모아서 하나의 개념으로 묶으면 그녀석이다.
+  // 파악된것: 입력값 들어가는거 보니 서버 설정이고, 실제 사용되는거 보니 instance.get, instance.post를 하더라
+  // 기억할것: 그렇다면 instance라는 녀석은. 미리 설정값을 세팅해두는 녀석이고. 세팅된 녀석에다가 get, post를 쓰도록 사용하면 되겠구나.
+  const instance = axios.create({
+    baseURL: 'http://localhost:4000/',
+    // baseURL: 'http://192.168.0.6:3000/',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+
+  const userData = {
+    username: id,
+    password: pw,
   };
+
+  try {
+    const res = await instance.post('/login', userData);
+    console.log('결과', res.data);
+
+    if (res.data.success) {
+      console.log('로그인 성공');
+      return true;
+    } else {
+      console.log('로그인 실패');
+      return false;
+    }
+  } catch (error) {
+    console.log('실패함', error);
+    return false;
+  }
+};
+
+const handleLogin = async () => {
+  // 로그인 로직을 여기에 작성합니다.
+  console.log('로그인 시도:', getId, getPassword);
+  const isSucceed = await tryLogin(getId, getPassword);
+  // alert(`${getId}\n ${getPassword}\n${isSucceed}`);
+
+  if(isSucceed){
+    //로그인 성공시 처리
+    setLoginUserName('getId')
+
+  }else{
+    // 로그인 실패시 처리
+    alert(`${getId} 로그인에 실패했습니다.`);
+    setLoginUserName('')
+  }
+
+};
 
   const handleSignUp = () => {
     // 회원가입 로직 또는 페이지로의 이동을 여기에 작성합니다.
     console.log('회원가입 페이지로 이동');
   };
 
-  
   return (
-    <div className='login-form-container'>
-      <div className='input-container'>
-        <label htmlFor="id">ID :</label>
-        <input className='input-field'
-          type="text"
-          id="id"
-          value={getId}
-          onChange={(e) => setId(e.target.value)}
-        />
-      </div>
-      <div className='input-container'>
-        <label htmlFor="password">PW :</label>
-        <input className='input-field'
-          type="password"
-          id="password"
-          value={getPassword}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-      </div>
-      <div className='button-container'>
-        <button className='login-button' onClick={handleLogin}>로그인</button>
-        <button className='signup-button' onClick={handleSignUp}>회원가입</button>
-      </div>
+    <div>
+      {/* getLoginUserName 값이 있는 경우 */}
+      {getLoginUserName ? (
+        <div>
+          <div>{getLoginUserName}님 환영합니다.</div>
+        </div>
+      ) : (
+        // getLoginUserName 값이 없는 경우
+        <div>
+          <div>
+            <label htmlFor="id">ID :</label>
+            <input
+              type="text"
+              id="id"
+              value={getId}
+              onChange={(e) => setId(e.target.value)}
+            />
+          </div>
+          <div>
+            <label htmlFor="password">PW :</label>
+            <input
+              type="password"
+              id="password"
+              value={getPassword}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <div>
+            <button onClick={handleLogin}>로그인</button>
+            <button onClick={handleSignUp}>회원가입</button>
+          </div>
+        </div>
+      )}
     </div>
   );
+  
 }
 
 export default LoginForm;
