e8a01c3e0294700eee4453ec6dd997096f7a6deb
yeojin10044 <153582138+yeojin10044@users.noreply.github.com>
2024-05-02 13:12:27 +0900

    로그인, 챗봇

--- react-test/src/store.js
+++ react-test/src/store.js
@@ -3,7 +3,7 @@ import { configureStore, createSlice } from '@reduxjs/toolkit'
 
 let user =  createSlice({
     name : 'user1', // userState() 역할임
-    initialState : {userId : 'kim'}, // state하나를 slice라고 부름
+    initialState : {userId : ''}, // state하나를 slice라고 부름
       reducers : {
     changeName(state, action){
       const userId = action.payload
