c1e3150451379856d0a34136c091c81136272c53
WonBinPark <57087624+ParkWonBin@users.noreply.github.com>
2024-05-03 18:48:20 +0900

    쿼리등록

---
+++ _main/db-main/createTables.sql
@@ -0,0 +1,43 @@
+--- DROP
+DROP TABLE users;
+DROP TABLE posts;
+DROP TABLE comments;
+
+--- CREATE
+CREATE TABLE users (
+    idx NUMBER PRIMARY KEY,
+    u_id VARCHAR2(100),
+    u_name VARCHAR2(100),
+    u_pw VARCHAR2(255)
+);
+
+CREATE TABLE posts (
+    idx NUMBER PRIMARY KEY,
+    p_title VARCHAR2(255),
+    p_content CLOB,
+    p_author NUMBER,
+    CONSTRAINT fk_post_author
+        FOREIGN KEY (p_author)
+        REFERENCES users(idx)
+        ON DELETE SET NULL  
+);
+
+CREATE TABLE comments (
+    idx NUMBER PRIMARY KEY,
+    c_content CLOB,
+    c_author NUMBER,
+    c_post NUMBER,
+    CONSTRAINT fk_comment_author
+        FOREIGN KEY (c_author)
+        REFERENCES users(idx)
+        ON DELETE SET NULL,
+    CONSTRAINT fk_comment_post
+        FOREIGN KEY (c_post)
+        REFERENCES posts(idx)
+        ON DELETE SET NULL  
+);
+
+-- READ
+select * from users;
+select * from posts;
+select * from comments;
